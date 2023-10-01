086a495bfe313ba842df719929e85535c5342c24
2018167552@qq.com <397360568@qq.com>
2023-10-01 15:19:07 +0800

    create form
    create form

---
+++ tdesign/desktop/form/Style/index.ts
@@ -0,0 +1,6 @@
+import theme from '../../_common/style/web/theme/_index.less'
+import theme from '../../_common/style/web/theme/_font.less'
+import formStyle from 
+'../../_common/style/web/components/form/_index.less'
+import formVar from 
+'../../_common/style/web/components/form/_var.less'
